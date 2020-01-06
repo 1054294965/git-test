919ae2fd67a15fee83a43d1f7bb1d93802077311
chushiyun <1054294965@qq.com>
2020-01-07 06:31:31 +0800

    11111111

--- controller.java
+++ controller.java
@@ -13,14 +13,15 @@ import java.util.Random;
 public class CacheServiceImpl {
     @Cacheable(value="cache1")// cache1
     public int get() {
+      //local 1
         int num = new Random().nextInt();
-        // modify1
+        //local 2
         return num;
     }
     @Cacheable(value="cache2")// cache2
     public int get2() {
+      //local 3
         int num = new Random().nextInt();
-        // modify2
         return num;
     }
     @CachePut(value={"cache1"})// 更新
